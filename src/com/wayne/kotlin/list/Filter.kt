11d4512a8727274474b2f6a1ecabd6348cceaeb8
wayne.han <wayne.han@picovr.com>
2020-10-29 18:35:48 +0800

    Collection high order function

--- src/com/wayne/kotlin/list/Filter.kt
+++ src/com/wayne/kotlin/list/Filter.kt
@@ -2,9 +2,12 @@ package com.wayne.kotlin.list
 
 /**
  * 几个集合中常用的高阶函数
- *      1.过滤filter
- *      2.映射map
- *      3.聚合reduce
+ *      1. filter：过滤
+ *      2. map/flatMap：映射
+ *      3. sum：所有元素求和
+ *      4. reduce：元素依次按规则聚合，结果与元素类型一致
+ *      5. fold：给定初始值，元素依次按规则聚合，结果与初始值类型一致
+ *
  */
 fun main() {
 
@@ -16,26 +19,48 @@ fun main() {
     }.forEach {
         println("过滤后循环遍历list：element=$it")
     }
+    val newList = list.map {
+        it + it
+    }
+    println(newList)
+    val newNewList = list.flatMap {
+        0 until 3
+    }
+    println(newNewList)
 
     println("-----------------------------------")
 
-    val map = mapOf(1 to "11", 2 to "22")
+    val map = mapOf(1 to "11", 2 to "22", 3 to "33")
     map.filter {
         it.value.startsWith("2")
     }.forEach {
-        println("过滤后循环遍历map,：key=${it.key}, element =${it.value}")
+        println("过滤后循环遍历map：key=${it.key}, element =${it.value}")
     }
-    map.map {
+    val value = map.map {
         it.key
-    }.forEach {
-        println(it)
-    } 
+    }
+    println(value)
+
+    println("-----------------------------------")
+
     val sum = map.map {
         it.key
-    }.reduce { total, score ->
-        total + score
-    }
+    }.sum()
     println(sum)
+
+    val reduce = map.map {
+        it.key
+    }.reduce { acc, i ->
+        acc + i
+    }
+    println(reduce)
+
+    val fold = map.map {
+        it.key
+    }.fold("结果是：") { acc, i ->
+        acc + i
+    }
+    println(fold)
 
 
 }
