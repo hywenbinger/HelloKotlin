27dc8c7ba7d414d241be540fc38e161f8ab6cb51
wayne.han <wayne.han@picovr.com>
2020-10-30 15:12:16 +0800

    see DSL

--- src/com/wayne/kotlin/TestDSL.kt
+++ src/com/wayne/kotlin/TestDSL.kt
@@ -19,6 +19,22 @@ package com.wayne.kotlin
  *          Groovy/Kotlin配置
  *          支持多种仓库
  *          易扩展高性能
+ *
+ * 安装Gradle
+ *      1.直接下载解压
+ *      2.通过包管理工具
+ *          SDKMAN：sdk install gradle
+ *          Homebrew：brew install gradle
+ *      3.Gradle Wrapper
+ *          每个Gradle工程可以灵活配置Gradle版本
+ *          无需手动下载安装，配置版本即可自动下载
+ *
+ * 认识Gradle配置
+ *      1.构建仓库插件：去哪找构建插件
+ *      2.构建仓库配置：找哪些构建插件
+ *      3.工程依赖仓库：去哪找工程依赖
+ *      4.工程依赖配置：找哪些工程依赖
+ *      5.任务管理：如何构建
  */
 fun main() {
     // 体验Gradle Kotlin DSL
