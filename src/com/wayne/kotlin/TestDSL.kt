47c0e1d0d58c7066912fa5c1b2a0dd5db211c2f9
wayne.han <wayne.han@picovr.com>
2020-10-30 14:56:49 +0800

    see DSL

---
+++ src/com/wayne/kotlin/TestDSL.kt
@@ -0,0 +1,25 @@
+package com.wayne.kotlin
+
+/**
+ * DSL
+ *      1.Domain Specific Language
+ *      2.领域特定语言
+ *      3.专注于【特定应用程序领域】的计算机语言
+ *      4.代表语言：SQL、Gradle、Kotlin
+ *
+ * 构建工具的进化：
+ *      1.Ant
+ *          XML配置
+ *          手动管理依赖
+ *      2.Maven
+ *          XML配置
+ *          中央仓库
+ *          自动管理依赖
+ *      3.Gradle
+ *          Groovy/Kotlin配置
+ *          支持多种仓库
+ *          易扩展高性能
+ */
+fun main() {
+    // 体验Gradle Kotlin DSL
+}
